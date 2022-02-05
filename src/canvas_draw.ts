85c1f159b18edcdbd56f1cb1759cc671fe84bbc0
editorbank <>
2022-02-05 18:20:31 +0300

    remove dirt

--- src/canvas_draw.ts
+++ src/canvas_draw.ts
@@ -2,12 +2,6 @@ import { InData } from "./in_data";
 
 console.log("Hello from main_module.ts!");
 
-export function crop_string(s :string, maxLen :number = 0) :string {
-  if(s.length > maxLen && maxLen !== 0)
-    return s.substring(0,maxLen) + '...'
-  else
-    return s;
-}
 export function canvas_draw(canvas :HTMLCanvasElement, in_data :InData){
   if(!canvas) return;
   var opt :CanvasRenderingContext2DSettings = {alpha: true};
